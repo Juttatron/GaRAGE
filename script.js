7595989516657971d4a2de2b5c00bd79af91de29
Justin Phillips <jutty_8@hotmail.com>
2024-09-29 14:56:45 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', function(){
+    //Canvas setup
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1500;
+    canvas.height = 500;
+
+    class InputHandler {
+
+    }
+
+    class Projectile {
+
+    }
+
+    class Particle {
+
+    }
+
+    class Player {
+        constructor(game) {
+            this.game = game;
+            this.width = 190;
+            this.height = 120;
+        }
+    }
+
+    class Enemy {
+
+    }
+
+    class Layer {
+
+    }
+
+    class Background {
+
+    }
+
+    class UI {
+
+    }
+    
+    class Game {
+
+    }
+
+});
